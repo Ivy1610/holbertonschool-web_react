36fd27c6cb1b23840af3647cf9b60477997f2c2e
Ivy1610 <8682@holbertonschool.com>
2025-01-28 14:09:10 +0100

    Add TypeScript project structure with configuration files and initial implementation

---
+++ TypeScript/task0/js/main.ts
@@ -0,0 +1,49 @@
+// create interface Student
+
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+// create two student objects
+
+const student1: Student = {
+    firstName: 'Marie',
+    lastName: 'Curie',
+    age: 26,
+    location: 'New York'
+};
+
+const student2: Student = {
+    firstName: 'Albert',
+    lastName: 'Einstein',
+    age: 30,
+    location: 'Boston'
+};
+
+const studentsList: Student[] = [student1, student2];
+
+// create table and render students fistName and location
+
+const table = document.createElement('table');
+const tableBody = document.createElement('tablebody');
+
+studentsList.forEach((student) => {
+    const row = document.createElement('tr');
+
+    const firstNameCell = document.createElement('td');
+    firstNameCell.textContent = student.firstName;
+    row.appendChild(firstNameCell);
+
+
+    const locationCell = document.createElement('td');
+    locationCell.textContent = student.location;
+    row.appendChild(locationCell);
+
+    tableBody.appendChild(row);
+});
+
+table.appendChild(tableBody);
+document.body.appendChild(table);
