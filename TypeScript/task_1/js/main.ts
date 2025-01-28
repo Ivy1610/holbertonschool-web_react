e66d347d3a5a174b5bd87a7d07c699cd9aca7d9a
Ivy1610 <8682@holbertonschool.com>
2025-01-28 14:55:21 +0100

    Add Directors interface and update main.ts to include director example

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -6,14 +6,28 @@ interface Teacher {
     yearsOfExperience?: number;  // Optional property
     location: string;
     [propName: string]: any;
-  }
+}
 
-  const teacher3: Teacher = {
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+const teacher3: Teacher = {
     firstName: 'John',
     lastName: 'Doe',
     fullTimeEmployee: false,
     location: 'London',
     contract: false,
+};
+
+console.log(teacher3);
+
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
   };
   
-  console.log(teacher3);
+  console.log(director1);
